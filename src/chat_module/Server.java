c05443f1e18c87b42529151257cd008c09d97ab0
mamerisawesome <atmalmer23@gmail.com>
2016-11-17 15:14:28 +0800

    "Ligaw na tab"

--- src/chat_module/Server.java
+++ src/chat_module/Server.java
@@ -35,7 +35,7 @@ public class Server {
                 }
               }
             };
-              send.start();
+            send.start();
           }
         } catch (Exception e) {
           e.printStackTrace();
